22e067cb624e504483d0299d360c5699d864364a
iwasemo <iwasemo@bc.edu>
2025-01-21 10:56:01 -0500

    Completed challenge: SwiftUI Challenge: The Elvis Costello Peace, Love, and Understanding App

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .foregroundStyle(.purple)
+            Spacer()
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            Text(message)
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .foregroundStyle(.purple)
+            Spacer()
+            HStack {
+                Button("Peace") {
+                    imageName = "peacesign"
+                    message = "peace"
+                }
+                Button("Love") {
+                    imageName = "heart"
+                    message = "love"
+                }
+                Button("Understanding") {
+                    imageName = "lightbulb"
+                    message = "understanding"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
         }
         .padding()
     }
