767d3d3ec7b12121f02e212823109177a9cf00b5
iwasemo <iwasemo@bc.edu>
2025-01-21 10:40:12 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Momoka Iwase on 2025/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
